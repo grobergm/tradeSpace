8d93f127e3841786a68290f3f911c6e67fd474b0
grobergm <grobergmg@gmail.com>
2025-03-03 21:10:13 -0800

    add nav with placeholder routes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-
+import Nav from './components/Nav/Nav.vue';
 </script>
 
 <template>
   <div class="appWrapper">
+    <Nav></Nav>
     <RouterView />
   </div>
 </template>
 
 <style scoped>
 
+.appWrapper {
+  max-width: 80rem;
+  margin:0 auto;
+  padding:0 1rem;
+}
 </style>
---
+++ src/components/Nav/Nav.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import User from '../People/User.vue';
+
+const user:User = {
+  userId:1,
+  name: "John Doe",
+  email: "johndoe@email.com",
+  photoUrl: "/img/user_thumbnail.jpg"
+
+}
+</script>
+
+<template>
+<nav>
+  <ul>
+    <li>
+      <RouterLink to="/">
+        <img src="/icons/LOGO.svg"/>
+      </RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/people/1">
+        <User :user="user"></User>
+      </RouterLink>
+    </li>
+  </ul>
+</nav>
+</template>
+
+<style scoped>
+
+
+ul {
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+  margin:0;
+  padding:0;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  color:white;
+  text-decoration: none;
+}
+</style>
